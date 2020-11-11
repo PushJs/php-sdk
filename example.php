eab664bd4e730c7999a58cc442cbfd0b5dc70f53
Willem Daems <willem@dotready.nl>
2020-11-11 20:41:41 +0100

    wip

--- example.php
+++ example.php
@@ -2,6 +2,8 @@
 
 require "vendor/autoload.php";
 
+use \pushjs\Service\PushJS;
+
 $host = '192.168.56.105';
 $port = 9100;
 $domain = 'bijons';
@@ -10,7 +12,7 @@ $channelId = 'BATTLE_CHANNEL';
 
 $key = 'eba02592-2b87-437b-b363-766cbd87230e';
 
-$pushJS = new \pushjs\Service\PushJS($key, $host, $port, false);
+$pushJS = new PushJS($key, $host, $port, false);
 
 $pushJS->connect();
 
@@ -26,14 +28,5 @@ $pushJS->getEventManager()->dispatchEvent($channelId, 'CHAT_MESSAGE', json_encod
     ])
 );
 
-$pushJS->getEventManager()->dispatchPrivateEvent(77, 'CHAT_MESSAGE', json_encode([
-        'text' => 'BOOM!',
-        'foo' => true
-    ])
-);
-
-$pushJS->getEventManager()->dispatchFilteredEvent(77, 'CHAT_MESSAGE', json_encode([
-        'text' => 'BOOM!',
-        'foo' => true
-    ])
-);
+$pushJS->getEventManager()->dispatchPrivateEvent(77, 'CHAT_MESSAGE', 'this is a private message');
+$pushJS->getEventManager()->dispatchFilteredEvent($channelId, 'CHAT_MESSAGE', 'this should only go to to clients with attribute color and value red');
